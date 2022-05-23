2bf6a79aec6e98e38114be30db4ff2e613bb4b4f
hugofucksmann <hugoffuksmann@gmail.com>
2022-05-23 19:32:46 -0300

    agregado de nuevos componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import Text3d from "./mui-library_colodev/src/textEff/3dText.js/text3d";
-
 function App() {
   return (
     <>
       <div
         style={{
           height: "100vh",
-          overflow: "hidden",
-          width: "80%",
-        }}>
-        <Text3d>Colo Developer</Text3d>
-      </div>
+          //overflow: "hidden",
+          width: "100%",
+          // backgroundColor: "darkblue",
+        }}></div>
     </>
   );
 }
---
+++ src/mui-library_colodev/src/textEff/gradiantPurpleText/gradiantPurpleText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./gradiantPurpleText.css";
+
+const GradiantPurpleText = () => {
+  return (
+    <article>
+      <h1>
+        If it’s not got a gradient heading, is it even a tech tool website?
+      </h1>
+      <p>
+        But really, open the CSS panel for a nice little gradient you can use
+      </p>
+    </article>
+  );
+};
+
+export default GradiantPurpleText;
